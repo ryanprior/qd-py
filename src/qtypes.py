fef52641b2ff65e7b23ae96a205fa86610a4ebdc
Ryan Prior <ryanprior@hey.com>
2021-05-04 19:20:33 -0500

    Adds some basic data types

---
+++ src/qtypes.py
@@ -0,0 +1,22 @@
+from numpy import cdouble, array, dtype
+from dataclasses import dataclass
+
+# example:
+# k = ket(1,0)
+
+Ket = dtype([("zero", cdouble), ("one", cdouble)])
+# Transform = dtype((
+#     [("a", cdouble), ("b", cdouble)],
+#     [("c", cdouble), ("d", cdouble)]
+# ))
+
+def ket(zero: cdouble, one: cdouble) -> Ket:
+    return array(([zero], [one]), dtype=cdouble)
+
+@dataclass
+class Gate:
+    transform: array
+
+    def apply(self, k: Ket):
+        return self.transform * k
+
