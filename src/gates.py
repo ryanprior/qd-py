8085528e05950ed84b05294c12e909b0741c4045
Ryan Prior <ryanprior@hey.com>
2021-05-06 19:12:23 -0500

    Implements Hadamard gate

--- src/gates.py
+++ src/gates.py
@@ -2,12 +2,21 @@
 
 from .qtypes import ket, Gate
 from numpy import array
+from math import sqrt
 
 Not = Gate(transform=array(
     [(0, 1),
      (1, 0)]
 ))
 
+Hadamard = Gate(transform=1/sqrt(2) * array(
+    [(1,  1),
+     (1, -1)]
+))
+
 print(Not)
 print(ket(1,0))
+print("NOT==")
 print(Not.apply(ket(1, 0)))
+print("HADAMARD==")
+print(Hadamard.apply(ket(1, 0)))
